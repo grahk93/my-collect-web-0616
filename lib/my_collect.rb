1d1751db1a11eeaef8355412813cc9144f83ade2
grahk93 <grace.juster@flatironschool.com>
2016-06-04 13:18:49 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  new_array
+end
 
-
+my_collect(["Tim Jones,", "Tom Smith,", "Jim Campagno"]) do |x|
+  "#{x}"
+end
